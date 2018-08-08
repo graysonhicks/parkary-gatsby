866eb007f82dab998f116af746ca67ae80d03183
Grayson Hicks <graysonhicks@gmail.com>
2018-08-07 22:50:59 -0400

    starting page creation

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,9 @@
  */
 
 // You can delete this file if you're not using it
+
+exports.onCreateNode = ({ node }) => {
+  if (node.internal.type === `ContentfulPark`) {
+    console.log(node)
+  }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { graphql } from 'gatsby'
+
 // eslint-disable-next-line
 import globalStyles from '../styles'
 
