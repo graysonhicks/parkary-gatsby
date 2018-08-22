4bdff8e0f4debc1b52ca7a07dab618ce9dc1c166
Grayson Hicks <graysonhicks@gmail.com>
2018-08-22 18:50:05 -0400

    styled 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,11 +1,43 @@
 import React from 'react'
-import Layout from '../components/layout'
+import styled from 'styled-components'
+import Link from 'gatsby-link'
+
+import { Container, Card, Heading, Text } from 'rebass'
 
 const NotFoundPage = () => (
-  <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+  <NotFoundContainer>
+    <NotFoundCard>
+      <NotFoundHeading>404 - NOT FOUND</NotFoundHeading>
+      <NotFoundText>Sorry! Doesn't look like anything is here.</NotFoundText>
+      <NotFoundText>
+        Click <Link to="/">here</Link> to go back home.
+      </NotFoundText>
+    </NotFoundCard>
+  </NotFoundContainer>
 )
 
 export default NotFoundPage
+
+const NotFoundContainer = styled(Container)`
+  display: flex;
+  height: 100%;
+  width: 100%;
+  align-items: center;
+`
+
+const NotFoundCard = styled(Card)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 300px;
+  width: 100%;
+`
+
+const NotFoundHeading = styled(Heading)`
+  margin-bottom: 15px;
+`
+
+const NotFoundText = styled(Text)`
+  margin-bottom: 15px;
+`
